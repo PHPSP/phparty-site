81eb90cd79751502fb8e60d5958b77dd010598c8
Anderson Casimiro <o@duodra.co>
2015-10-21 08:56:48 -0200

    fix some app engine issues

--- data/events.js
+++ data/events.js
@@ -2,5 +2,5 @@ events = [
     {where :'PB', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'RJ', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'SC', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
-    {where :'SP', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
+    {where :'SP', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]}
 ];
