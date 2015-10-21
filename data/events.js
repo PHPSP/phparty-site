8aefb658086b233deeacf1f89ebc3babbd7d8bdc
João Paulo V. Martins <jp.joao@gmail.com>
2015-10-21 08:26:00 -0700

    Adiciona DF como participante

--- data/events.js
+++ data/events.js
@@ -2,5 +2,6 @@ events = [
     {where :'PB', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'RJ', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'SC', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
-    {where :'SP', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]}
+    {where :'SP', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
+    {where :'DF', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
 ];
