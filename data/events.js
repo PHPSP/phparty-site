d339efa61477ee05a4fa8a0e86badf19d2f1d9d6
Abdala Cerqueira <abdala.cerqueira@gmail.com>
2015-10-30 18:53:52 -0300

    Adicionando informações do evento na Paraíba

--- data/events.js
+++ data/events.js
@@ -1,5 +1,5 @@
 events = [
-    {where :'PB', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
+    {where :'PB', desc : '(Às 9h, no IFPB*. Será realizada uma batalha de novas funcionalidades. Qual será a sua favorita?', events:[{title: 'Inscrições', link: 'https://phppb.typeform.com/to/ynrUvl'}]},
     {where :'RJ', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'SC', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'SP', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
