8840714ce23262ce3db296e5fbc55861db211d15
João Paulo V. Martins <jp.joao@gmail.com>
2015-11-03 11:38:03 -0200

    Arruma links que não abriam em nova janela

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,3 +1,3 @@
 sponsors =[
-    {name: "PHP", url: 'http://www.php.net', logo: "http://php.net/images/logos/php-med-trans.png"},
+    {name: "PHP", link: 'http://www.php.net', logo: "http://php.net/images/logos/php-med-trans.png"},
 ];
