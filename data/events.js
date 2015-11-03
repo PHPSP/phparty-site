f11c6f086fd54cec698a25aec4c8a7f354eace9a
Anderson Casimiro <o@duodra.co>
2015-11-03 10:07:31 -0200

    Adding RJ event

--- data/events.js
+++ data/events.js
@@ -1,6 +1,6 @@
 events = [
     {where :'PB', desc : '(Às 9h, no IFPB*. Será realizada uma batalha de novas funcionalidades. Qual será a sua favorita?', events:[{title: 'Inscrições', link: 'https://phppb.typeform.com/to/ynrUvl'}]},
-    {where :'RJ', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
+    {where :'RJ', desc : 'A Festa de Lançamento do PHP7, na Cidade Maravilhosa', events:[{title: 'Mais detalhes', link: 'https://doity.com.br/phparty7-rj'}]},
     {where :'SC', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'SP', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'DF', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
