c43f0562d0462f2abbafd2abbd9f7ccd47fcebed
João Paulo V. Martins <jp.joao@gmail.com>
2015-11-04 17:13:30 -0200

    Adiciona Dark Mira Brasil como patrocinadora (PB)

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,3 +1,4 @@
 sponsors =[
     {name: "PHP", link: 'http://www.php.net', logo: "http://php.net/images/logos/php-med-trans.png"},
+    {name: "Dark Mira Brasil", link: 'http://www.darkmira.com.br', logo: "http://www.darkmira.com.br/wp-content/uploads/2014/04/DMB_logo_400x301-v2.png"},
 ];
