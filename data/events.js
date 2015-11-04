042a6be2fff0fb1435ad0e1bd2d1cdf9180c9973
Abdala Cerqueira <abdala.cerqueira@gmail.com>
2015-11-04 18:38:31 -0300

    Update events.js

--- data/events.js
+++ data/events.js
@@ -1,5 +1,5 @@
 events = [
-    {where :'PB', desc : '(Às 9h, no IFPB*. Será realizada uma batalha de novas funcionalidades. Qual será a sua favorita?', events:[{title: 'Inscrições', link: 'https://phppb.typeform.com/to/ynrUvl'}]},
+    {where :'PB', desc : 'Às 9h, no IFPB. Será realizada uma batalha de novas funcionalidades. Qual será a sua favorita?', events:[{title: 'Inscrições', link: 'https://phppb.typeform.com/to/ynrUvl'}]},
     {where :'RJ', desc : 'A Festa de Lançamento do PHP7, na Cidade Maravilhosa', events:[{title: 'Mais detalhes', link: 'https://doity.com.br/phparty7-rj'}]},
     {where :'SC', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
     {where :'SP', desc : 'Detalhes em breve.', events:[{title: 'Em Breve', link: '#'}]},
