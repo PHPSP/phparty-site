99cbe459deaa752cd16aac9d8b98c9ce046c9fd8
João Paulo V. Martins <jp.joao@gmail.com>
2015-11-10 21:04:34 -0200

    Move os links e imagens dos patrocinadores para https

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,4 +1,4 @@
 sponsors =[
-    {name: "PHP", link: 'http://www.php.net', logo: "http://php.net/images/logos/php-med-trans.png"},
-    {name: "Dark Mira Brasil", link: 'http://www.darkmira.com.br', logo: "http://www.darkmira.com.br/wp-content/uploads/2014/04/DMB_logo_400x301-v2.png"},
+    {name: "PHP", link: 'https://www.php.net', logo: "https://php.net/images/logos/php-med-trans.png"},
+    {name: "Dark Mira Brasil", link: 'https://www.darkmira.com.br', logo: "https://www.darkmira.com.br/wp-content/uploads/2014/04/DMB_logo_400x301-v2.png"},
 ];
