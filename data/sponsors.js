98264cb4ce443a72484fae24ffb6147e18f4fadc
João Paulo V. Martins <jp.joao@gmail.com>
2015-11-11 11:24:33 -0200

    Diferencia Patrocinadores ($$$$) de Apoiadores...

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,7 +1,3 @@
 sponsors =[
-    {name: "PHP", link: 'https://www.php.net', logo: "https://php.net/images/logos/php-med-trans.png"},
     {name: "Dark Mira Brasil", link: 'https://www.darkmira.com.br', logo: "https://www.darkmira.com.br/wp-content/uploads/2014/04/DMB_logo_400x301-v2.png"},
-    {name: "Nasajon Educacional", link: 'http://www.nasajoneducacional.com.br/', logo: "https://encrypted-tbn1.gstatic.com/images?q=tbn%3AANd9GcQZOxi66UZ6PFRUIltM4fs-9cKNWmKCNA9GENjHHYHBkdQzXKoMF2VjZs2m"},
-    {name: "Novatec Editora", link: 'https://www.novatec.com.br/', logo: "https://pbs.twimg.com/profile_images/458961077972123648/i5UzTEx7.jpeg"},
-    {name: "Tecnologia em Sistemas para Internet", link: 'http://www.ifpb.edu.br/campi/joao-pessoa/cursos/cursos-superiores-de-tecnologia/sistemas-para-internet', logo: "http://phpeste.net/img/ifpb-tsi.png"},
 ];
